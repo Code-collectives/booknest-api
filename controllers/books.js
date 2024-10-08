171bf81600c30d918b8ebc59f368f5db9375714d
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-10-08 11:21:07 +0000

    almost done

--- controllers/books.js
+++ controllers/books.js
@@ -1,11 +1,11 @@
 // controllers/bookController.mjs
-import Book from '../models/bookModel.js';
+import Book from '../models/books.js';
 
 // Add a new book
 export const addBook = async (req, res) => {
   const book = new Book(req.body);
   await book.save();
-  res.send(book);
+  res.status(201).send('Book Added!'); // Send a 201 status for creation
 };
 
 // Get all books
@@ -13,23 +13,25 @@ export const getAllBooks = async (req, res) => {
   const books = await Book.find();
   res.send(books);
 };
-
 // Get book by ID
 export const getBookById = async (req, res) => {
   const book = await Book.findById(req.params.id);
+  if (!book) return res.status(404).send('Book not found');
   res.send(book);
 };
 
 // Update book
 export const updateBook = async (req, res) => {
   const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!book) return res.status(404).send('Book not found');
   res.send(book);
 };
 
 // Delete book
 export const deleteBook = async (req, res) => {
-  await Book.findByIdAndDelete(req.params.id);
-  res.send('Book deleted');
+  const book = await Book.findByIdAndDelete(req.params.id);
+  if (!book) return res.status(404).send('Book not found');
+  res.send('Book deleted'); // Response indicating successful deletion
 };
 
 
