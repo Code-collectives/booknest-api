a6337bc9a8b769ecb26e355e5b16d0423aa8670d
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-10-09 16:43:47 +0000

    updated new index with routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bookRoutes from './routes/books.js';
+import authorRouter from './routes/authors.js';
+import reviewRouter from './routes/review.js';
+
+
 
 
 
@@ -8,17 +12,21 @@ import bookRoutes from './routes/books.js';
 const app = express();
 app.use(express.json());
 
+
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
   });
+
   
 // Connect to MongoDB
 await mongoose.connect(process.env.MONGO_URI);
 
 // Use the book routes
 app.use( bookRoutes);
+app.use( authorRouter);
+app.use( reviewRouter);
 
 // Start the server
 const port = 3007;
