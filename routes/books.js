8e7d29625b9a9e017c258717da761f04f665fa8e
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-10-09 11:36:55 +0000

    changed put to patch

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/books', addBook);         // Add new book
 router.get('/books', getAllBooks);      // Get all books
 router.get('/books/:id', getBookById);  // Get book by ID
-router.put('/books/:id', updateBook);   // Update book by ID
+router.patch('/books/:id', updateBook);   // Update book by ID
 router.delete('/books/:id', deleteBook); // Delete book by ID
 
 export default router;
