adf9e04f07b1d668d55da5bd6f7a48e18dddfd9b
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-10-09 16:39:03 +0000

    new index file

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bookRoutes from './routes/books.js';
+
+
+
+
+const app = express();
+app.use(express.json());
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  });
+  
+// Connect to MongoDB
+await mongoose.connect(process.env.MONGO_URI);
+
+// Use the book routes
+app.use( bookRoutes);
+
+// Start the server
+const port = 3007;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
